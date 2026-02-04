e21a6a8d04284447e002042af90318419ef558ae
Syed N. Abbas <sayyadnaveedabbas@gmail.com>
2026-02-04 11:44:02 +0500

    Added Button Tiles

--- RestaurantPOS/ViewModels/Home/HomeViewModel.cs
+++ RestaurantPOS/ViewModels/Home/HomeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using RestaurantPOS.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.Input;
+using RestaurantPOS.Services;
+using RestaurantPOS.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,27 @@ namespace RestaurantPOS.ViewModels.Home
 {
     public class HomeViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+
+        public IRelayCommand DineInCommand { get; }
+        public IRelayCommand TakeawayCommand { get; }
+        public IRelayCommand DeliveryCommand { get; }
+
+        public HomeViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            DineInCommand = new RelayCommand(() => Navigate("DineIn"));
+            TakeawayCommand = new RelayCommand(() => Navigate("Takeaway"));
+            DeliveryCommand = new RelayCommand(() => Navigate("Delivery"));
+        }
+
+        private void Navigate(string mode)
+        {
+            // Later:
+            // - Tables for DineIn
+            // - Order screen for Takeaway / Delivery
+        }
+
     }
 }
