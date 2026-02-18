c8e72c7886e1e1e216e8cf1a788ac949a0613c6a
Syed N. Abbas <sayyadnaveedabbas@gmail.com>
2026-02-18 15:17:27 +0500

    UsersView UI Enhancements

---
+++ RestaurantPOS/UI/Converters/StatusToBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RestaurantPOS.UI.Converters
+{
+    public class StatusToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isActive)
+            {
+                // Green for active, Muted Gray for inactive
+                return isActive
+                    ? new SolidColorBrush((Color)ColorConverter.ConvertFromString("#16A34A"))
+                    : new SolidColorBrush((Color)ColorConverter.ConvertFromString("#94A3B8"));
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
